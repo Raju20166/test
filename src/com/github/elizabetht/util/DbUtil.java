8be9ae79edc145af1b1bd8eeb8bfc8be0a245712
anilcpatil <anil.patil@synerzip.com>
2017-02-17 18:27:57 +0530

    Update DbUtil.java

--- src/com/github/elizabetht/util/DbUtil.java
+++ src/com/github/elizabetht/util/DbUtil.java
@@ -26,8 +26,24 @@ public class DbUtil {
 					String password = properties.getProperty("password");
 
 					Class.forName("com.mysql.jdbc.Driver").newInstance();
-					dbConnection = DriverManager.getConnection("jdbc:mysql://172.25.25.16:3306/studentEnrollment", "pk", "pk");
+					//dbConnection = DriverManager.getConnection("jdbc:mysql://172.25.25.16:3306/studentEnrollment", "pk", "pk");
 					//dbConnection = DriverManager.getConnection(connectionUrl,userName, password);
+					
+					System.setProperty("javax.net.ssl.keyStore","/home/ubuntu/keystore");
+					System.setProperty("javax.net.ssl.keyStorePassword","password");
+					System.setProperty("javax.net.ssl.trustStore","/home/ubuntu/truststore");
+					System.setProperty("javax.net.ssl.trustStorePassword","password");
+					
+					String url = "jdbc:mysql://172.25.25.20:3306/studentEnrollment"+"?"+
+									"verifyServerCertificate=true"+
+									"&useSSL=true"+
+									"&requireSSL=true";
+			
+					String user = "anilpatil";
+					String password = "password";
+
+					Class dbDriver = Class.forName("com.mysql.jdbc.Driver");
+					con = DriverManager.getConnection(url, user, password);
 				}
 			} catch (Exception e) {
 				e.printStackTrace();
